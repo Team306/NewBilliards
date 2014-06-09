b691e3b37d749e4f335b713241ca058e0694425c
fdchao <823267971@qq.com>
2014-06-09 22:16:24 +0800

    network little modification

--- glwindow.cpp
+++ glwindow.cpp
@@ -101,8 +101,7 @@ void GLWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if(game.getPlayerFlag() == LOCAL || game.getGameMode() != NETWORK_MODE || game.getGameMode() == END_FRAME){
         game.setMousePosition(Vector2(event->x(), event->y()));
-        if(game.getGameMode() == NETWORK_MODE && (game.getGameState() == WAIT_FOR_STROKE || game.getGameState() == FREE_BALL
-                                                  ||game.getGameState() == BALL_IS_RUNNING)){
+        if(game.getGameMode() == NETWORK_MODE && (game.getGameState() == WAIT_FOR_STROKE || game.getGameState() == FREE_BALL)){
             SendData.clear();
             SendData.append(QString("P#"));
             SendData.append(QString::number(event->x()));
